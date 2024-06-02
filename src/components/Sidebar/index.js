9dff1a8ff3bc248b0e530548ed3c8a48d937a0cf
Larkin Campbell <Larkincampbell24@gmail.com>
2024-06-02 16:51:10 -0700

    letter import functioning

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import './index.scss'
 import {Link} from 'react-router-dom'
-import LetterL from '../../assets/images/images/LetterL.png'
+import LetterL from '../../assets/images/LetterL.png'
+
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
