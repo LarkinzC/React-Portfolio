80bacd5b815aca337af104dfdb0aa68bc56e52b7
Larkin Campbell <Larkincampbell24@gmail.com>
2024-06-02 15:05:14 -0700

    scss work

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import {Link} from 'react-router-dom'
+import LetterL from '../../assets/images/images/LetterL.png'
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LetterL} alt='Logo' />
+        </Link>
+    </div>
+)
+
+export default Sidebar
