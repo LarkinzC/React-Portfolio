fa1bbbb1798ae566e22252c285e8f473cd4512e2
Larkin Campbell <Larkincampbell24@gmail.com>
2024-06-04 14:09:22 -0700

    implementing letter animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,37 @@
 import LetterL from '../../assets/images/LetterL.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import {useState, useEffect} from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['L', 'a', 'r', 'k', 'i', 'n', ' ', 'C', 'a', 'm', 'p', 'b', 'e', 'l', 'l']
+    const jobArray = ['W', 'e','b',' ', 'D','e','v','e','l','o','p','e','r','.']
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, [4000])
+    }, [])
+
     return (
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1>
-                    Hi, I'm
-                    <span className="name" style={{color: '#ffd700'}}>
-                        {/* <img src={LetterL} alt='developer' />  SOURCE HELP WITH FORMATTING INLINEI WITH SPAN*/}
-                        Larkin Campbell
-                    </span>
-                    Web Developer
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span> 
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span> 
+                    <span id='m' className={`${letterClass} _14`}>'m</span> 
+                        {/* <img src={LetterL} alt='developer' />  SOURCE HELP WITH FORMATTING INLINE WITH SPAN*/}
+                        <AnimatedLetters letterClass={letterClass} 
+                        strArray={nameArray} 
+                        idx={15} />
+                         <br />
+                         <AnimatedLetters letterClass={letterClass} 
+                        strArray={jobArray} 
+                        idx={30} />
                 </h1>
                 <h2>Frontend Developer / Javascript Expert / Pro DiscGolfer</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
